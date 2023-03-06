1d04b949ac3f45eeedd1aafa0b5ba5ceb93890f5
Deng.Zixu <admin@dengzixu.com>
2023-03-07 01:06:14 +0800

    🔧 优化构建配置

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import viteCompression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue(), viteCompression(),],
+    plugins: [vue()],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
@@ -15,11 +15,15 @@ export default defineConfig({
     build: {
         rollupOptions: {
             output: {
+                assetFileNames: 'assets/[hash][extname]',
+                chunkFileNames: 'chunks/[hash].js',
+                entryFileNames: 'entries/[hash].js',
                 manualChunks: {
                     vue: ['vue', 'vue-router'],
                     antdv: ['ant-design-vue'],
                     antdv_icon: ['@ant-design/icons-vue']
-                }
+                },
+                sourcemap: false,
             }
         }
     }
