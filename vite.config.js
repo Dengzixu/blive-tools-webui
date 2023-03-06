b88f4fa10b1deca4dd2d896d552017047bf92ba8
Deng.Zixu <admin@dengzixu.com>
2023-03-07 00:36:03 +0800

    🔧 优化构建配置，缩小输出文件体积

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,26 @@
-import { fileURLToPath, URL } from 'node:url'
+import {fileURLToPath, URL} from 'node:url'
 
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import viteCompression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [vue(), viteCompression(),],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    vue: ['vue', 'vue-router'],
+                    antdv: ['ant-design-vue'],
+                    antdv_icon: ['@ant-design/icons-vue']
+                }
+            }
+        }
     }
-  }
 })
