9fcf149b7dc74b24e5efb0b6c6f6c28a3e43cb3f
Deng.Zixu <admin@dengzixu.com>
2023-03-29 01:55:55 +0800

    🎨 优化礼物过滤器

--- src/api/BLiveAPIProxy.js
+++ src/api/BLiveAPIProxy.js
@@ -15,15 +15,28 @@ export default {
             }
         });
     },
-    giftDataProxy(roomID = 0) {
+    giftDataProxy(roomID = 0, areaID = 0, areaParentID = 0) {
+        let params;
+        if (areaID && areaID > 0) {
+            params = {
+                "platform": "pc",
+                "room_id": roomID,
+                "area_id": areaID,
+                "area_parent_id": areaParentID
+            }
+        } else {
+            params = {
+                "platform": "pc",
+                "room_id": roomID,
+            }
+        }
+
+
         return axios({
             url: "/proxy/https://api.live.bilibili.com/xlive/web-room/v1/giftPanel/giftData",
             baseURL: import.meta.env.VITE_PROXY_URL,
             method: "get",
-            params: {
-                "platform": "pc",
-                "room_id": roomID
-            }
+            params: params
         });
     },
     getWebAreaListProxy() {
