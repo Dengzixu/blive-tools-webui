e40696cdabeefe3a9dcf0cb39007987d46d6ed30
Deng.Zixu <admin@dengzixu.com>
2023-08-01 15:57:46 +0800

    🔧 优化构建配置

--- vite.config.ts
+++ vite.config.ts
@@ -12,5 +12,20 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[hash][extname]',
+        chunkFileNames: 'chunks/[hash].js',
+        entryFileNames: 'entries/[hash].js',
+        manualChunks: {
+          vue: ['vue', 'vue-router'],
+          antdv: ['ant-design-vue'],
+          antdv_icon: ['@ant-design/icons-vue']
+        },
+        sourcemap: false,
+      }
+    }
   }
 })
